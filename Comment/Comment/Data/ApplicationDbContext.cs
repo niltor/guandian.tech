4a27254831bcd78ff72957968aceaf7ca7ecb6aa
zpty still <zpty@outlook.com>
2018-06-12 06:49:31 +0800

    添加基本实体类

--- Comment/Comment/Data/ApplicationDbContext.cs
+++ Comment/Comment/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Comment.Data.Entity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,10 @@ namespace Comment.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Comment.Data.Entity.Comment> Comments { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ Comment/Comment/Data/Entity/Db.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comment.Data.Entity
+{
+    public class Db
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+        public DateTime UpdatedTime { get; set; } = DateTime.Now;
+    }
+}
