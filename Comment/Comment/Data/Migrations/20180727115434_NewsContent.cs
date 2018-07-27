2cd3e7057e2a87792ba32fd5c87b9876655e113c
zpty still <zpty@outlook.com>
2018-07-27 20:01:08 +0800

    内容字段长度修改

---
+++ Comment/Comment/Data/Migrations/20180727115434_NewsContent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Comment.Data.Migrations
+{
+    public partial class NewsContent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "News",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 4000,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "News",
+                maxLength: 4000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
