3d92ff4c1bba0afa08139a4e613f72a2f0ccf6e3
NilTor <zpty@outlook.com>
2018-11-01 19:01:16 +0800

    数据库 字段更新

---
+++ Comment/Guandian/Data/Migrations/20181101110055_Thumbnail.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Guandian.Data.Migrations
+{
+    public partial class Thumbnail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Thumbnail",
+                table: "Articles",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Thumbnail",
+                table: "Articles");
+        }
+    }
+}
