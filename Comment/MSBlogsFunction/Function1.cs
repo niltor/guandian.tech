9f3cb15b500e156de659e7e4b4c7ba3d04160bec
NilTor <zpty@outlook.com>
2018-11-12 10:01:47 +0800

    逐条提交

--- Comment/MSBlogsFunction/Function1.cs
+++ Comment/MSBlogsFunction/Function1.cs
@@ -7,7 +7,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using MSBlogsFunction.Entity;
 using MSBlogsFunction.Models;
-using MSBlogsFunction.RssFeeds;
 using Newtonsoft.Json;
 
 namespace MSBlogsFunction
@@ -17,8 +16,8 @@ namespace MSBlogsFunction
         //static readonly string baseApi = "http://localhost:3719/";
         static readonly string baseApi = "https://guandian.tech/";
         [FunctionName("MSBlogs")]
-        //public static async Task RunAsync([TimerTrigger("*/20 * * * * *")]TimerInfo myTimer, ILogger log)
-        public static async Task RunAsync([TimerTrigger("0 0 */6 * * *")]TimerInfo myTimer, ILogger log)
+        public static async Task RunAsync([TimerTrigger("*/20 * * * * *")]TimerInfo myTimer, ILogger log)
+        //public static async Task RunAsync([TimerTrigger("0 0 */6 * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
@@ -38,7 +37,6 @@ namespace MSBlogsFunction
         {
             string subKey = Environment.GetEnvironmentVariable("GoogleTranslateKey");
             var translateHelper = new TranslateTextHelper(subKey);
-            var tobeAddBlogs = new List<BlogForm>();
             log.LogInformation("采集微软RSS：" + blogs.Count + "条;\r\n" + string.Join(";\r\n", blogs.Select(b => b.Title).ToArray()));
             // blogs去重
             using (var hc = new HttpClient())
@@ -58,43 +56,50 @@ namespace MSBlogsFunction
 
                 if (blogs.Count > 0)
                 {
-                    var intelligence = new IntelligenceHelper();
-                    foreach (var item in blogs)
-                    {
-                        var thumbnail = await intelligence.GetImageFromTextAsync(item.Title);
-                        //var thumbnail = "";
 
-                        var blogForm = new BlogForm
+                    try
+                    {
+                        var intelligence = new IntelligenceHelper();
+                        foreach (var item in blogs)
                         {
-                            ContentEn = item.Content,
-                            AuthorName = item.Author,
-                            Categories = item.Categories,
-                            Content = translateHelper.TranslateText(item.Content),
-                            Summary = translateHelper.TranslateText(item.Description),
-                            Title = translateHelper.TranslateText(item.Title),
-                            TitleEn = item.Title,
-                            Link = item.Link,
-                            CreatedTime = item.CreateTime,
-                            Thumbnail = thumbnail
-                        };
-                        tobeAddBlogs.Add(blogForm);
+                            var thumbnail = await intelligence.GetImageFromTextAsync(item.Title);
+                            //var thumbnail = "";
+
+                            var blogForm = new BlogForm
+                            {
+                                ContentEn = item.Content,
+                                AuthorName = item.Author,
+                                Categories = item.Categories,
+                                Content = translateHelper.TranslateText(item.Content),
+                                Summary = translateHelper.TranslateText(item.Description),
+                                Title = translateHelper.TranslateText(item.Title),
+                                TitleEn = item.Title,
+                                Link = item.Link,
+                                CreatedTime = item.CreateTime,
+                                Thumbnail = thumbnail
+                            };
+                            var tobeAddBlogs = new List<BlogForm>
+                            {
+                                blogForm
+                            };
+                            response = await hc.PostAsJsonAsync(baseApi + "admin/tools/AddRssBlogs", tobeAddBlogs);
+                            if (response.IsSuccessStatusCode)
+                            {
+                                var result = await response.Content.ReadAsStringAsync();
+                            }
+                            else
+                            {
+                                log.LogError(response.StatusCode + response.ReasonPhrase);
+                            }
+                        }
                     }
+                    catch (Exception e)
+                    {
+
+                    }
+
                 }
 
-                // 发送请求入库
-                if (tobeAddBlogs.Count > 0)
-                {
-                    response = await hc.PostAsJsonAsync(baseApi + "admin/tools/AddRssBlogs", tobeAddBlogs);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var result = await response.Content.ReadAsStringAsync();
-                    }
-                    else
-                    {
-                        log.LogError(response.StatusCode + response.ReasonPhrase);
-                    }
-
-                }
             }
         }
     }
