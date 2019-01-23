32d82dba476ac88664b04ad033b4a18413fa0ca1
NilTor <zpty@outlook.com>
2019-01-23 23:33:11 +0800

    test

---
+++ Comment/Test/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Test
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+
+            var connString = "Server=localhost;User ID=root;Password=root;Database=test";
+
+            using (var conn = new MySqlConnection(connString))
+            {
+                await conn.OpenAsync();
+                // Retrieve all rows
+                using (var cmd = new MySqlCommand("SELECT * FROM hy_area", conn))
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                Console.WriteLine(reader.GetName(i) + ":" + reader.GetValue(i));
+                            }
+
+                        }
+                    }
+
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
