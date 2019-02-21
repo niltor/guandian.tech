24e2274d908fc4e6f60cf6248dfbed3bc4072e2a
NilTor <zpty@outlook.com>
2019-02-21 18:51:18 +0800

    待添加:webhook处理pull request

--- Comment/Guandian/Areas/Webhooks/Controllers/GithubController.cs
+++ Comment/Guandian/Areas/Webhooks/Controllers/GithubController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Octokit;
 
 namespace Guandian.Areas.Webhooks.Controllers
 {
@@ -12,6 +13,27 @@ namespace Guandian.Areas.Webhooks.Controllers
         public string Test()
         {
             return "test";
+        }
+
+        [HttpPost("pullrequest")]
+        public ActionResult PullRequestHook(PullRequestEventPayload pr)
+        {
+            switch (pr.Action)
+            {
+                case "closed":
+                    // TODO：用户端消息提醒
+                    break;
+                case "opened":
+                    // TODO:消息提醒
+                    break;
+                case "synchronize":
+                    // TODO: 处理pr 合并
+
+                    break;
+                default:
+                    break;
+            }
+            return Ok();
         }
     }
 }
