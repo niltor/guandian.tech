327a8ea4ce895ffcd8fcd8503c4794eb1ea1f3b8
NilTor <zpty@outlook.com>
2019-07-05 00:06:47 +0800

    调试微信推送

--- Comment/PushService/Function1.cs
+++ Comment/PushService/Function1.cs
@@ -7,17 +7,18 @@ namespace PushService
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
-        public static async void RunAsync([TimerTrigger("0 20 23 * * *")]TimerInfo myTimer, ILogger log)
+        [FunctionName("PushWeixinService")]
+        public static async void RunAsync([TimerTrigger("*/20 * * * * *")]TimerInfo myTimer, ILogger log)
+        //public static async void RunAsync([TimerTrigger("0 20 23 * * *")]TimerInfo myTimer, ILogger log)
         {
             int retryNum = 3;
             using (var hc = new HttpClient())
             {
-                var response = await hc.GetAsync("https://guandian.tech/weixin/Push/PushToWeixin");
+                var response = await hc.GetAsync("https://guandian.tech/weixin/Push/PushToWeixinAsync");
                 while (retryNum > 0 && !response.IsSuccessStatusCode)
                 {
                     retryNum--;
-                    response = await hc.GetAsync("https://guandian.tech/weixin/Push/PushToWeixin");
+                    response = await hc.GetAsync("https://guandian.tech/weixin/Push/PushToWeixinAsync");
                 }
             }
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
